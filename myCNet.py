01e418fe31f753979eb5973c769e138fbc34acf7
TianSen <414319563@qq.com>
2018-10-14 18:32:24 +0800

    Initial commit

---
+++ myCNet.py
@@ -0,0 +1,18 @@
+#搭建卷积神经网络，目的为了实现用脑电预测被试疲劳水平。使用kearas搭建
+#date：2018-10-14
+#Author：Sen Tian
+#email：414319563@qq.com
+import tensorflow as tf
+import numpy as np
+
+
+
+
+class CNeTS:
+
+
+    def __init__(self,shape):
+
+
+
+
